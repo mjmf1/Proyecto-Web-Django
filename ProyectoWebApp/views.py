1083e0fe7cdb1fb2525e2c9a5ae2b375796eb106
mjmf1 <marcomora61@gmail.com>
2025-11-26 21:06:38 -0600

    correcion de  flecos con sesiones y carro

--- ProyectoWebApp/views.py
+++ ProyectoWebApp/views.py
@@ -1,11 +1,12 @@
 
 from django.shortcuts import render, HttpResponse
 from Servicios.models import Servicio
-
+from carro.carro import Carro
 
 # Create your views here.
 
 def home(request):
+    carro = Carro(request) 
     return render(request, "ProyectoWebApp/home.html")
     #return HttpResponse("Home")
 
--- carro/context_processor.py
+++ carro/context_processor.py
@@ -1,6 +1,9 @@
 def importe_total_carro(request):
     total = 0
-    #if request.user.is_authenticated:
-    for key, value in request.session.get("carro", {}).items():
+    if request.user.is_authenticated:
+     for key, value in request.session.get("carro", {}).items():
             total = total + float(value["precio"])
+    else:
+       total = "Debe iniciar sesión"
+            
     return {"importe_total_carro": total}
